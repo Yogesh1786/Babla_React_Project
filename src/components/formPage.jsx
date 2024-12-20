d53d4cf2af6d5de5d811b7f21a618c9e042987c4
Yogesh <yogeshthakur11786@gmail.com>
2024-12-20 23:16:47 +0530

    New Chages

---
+++ src/components/formPage.jsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import styles from "./formPage.module.css";
+import { Button } from "@mui/material";
+
+function FormPage({ closeDialog }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    date: "",
+    message: "",
+  });
+
+  const [jsonData, setJsonData] = useState(null)//State to hold JSON data
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // You can also add logic to send this data to an API
+    // alert(`Form submitted with Name: ${formData.name}`);
+    // setFormData({ name: "", email: "", message: "",date: ""}); // Reset form
+    console.log("Form submitted:", formData);
+    setJsonData(formData)
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+      date: "",
+    });
+  };
+
+  const onCancel = () => {
+    closeDialog();
+  };
+
+  return (
+    <div className={styles.parentPage}>
+      <h1 className={styles.heading}>Form Page</h1>
+
+      <div>
+        <form className={styles.form} onSubmit={handleFormSubmit}>
+          <div className={styles.formGroup}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Date</label>
+            <input
+              type="date"
+              id="date"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className={styles.formGroup}>
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              placeholder="Enter your message"
+              rows="4"
+              required
+            ></textarea>
+          </div>
+
+          <div className={styles.footerBtns}>
+            <Button
+              size="large"
+              sx={{
+                backgroundColor: "#4a70c4",
+                color: "white",
+                textTransform: "none",
+                padding: "10px 30px",
+                borderRadius: "5px",
+                fontWeight: "500",
+                transition: "background-color 0.3s ease, transform 0.2s ease",
+                "&:hover": {
+                  backgroundColor: "#365a9c",
+                  transform: "translateY(-2px)",
+                },
+              }}
+            >
+              Submit
+            </Button>
+
+            <Button
+              size="large"
+              sx={{
+                backgroundColor: "#d9534f",
+                color: "white",
+                textTransform: "none",
+                padding: "10px 30px",
+                borderRadius: "5px",
+                fontWeight: "500",
+                transition: "background-color 0.3s ease, transform 0.2s ease",
+                "&:hover": {
+                  backgroundColor: "#c9302c",
+                  transform: "translateY(-2px)",
+                },
+              }}
+              onClick={onCancel}
+            >
+              Cancel
+            </Button>
+          </div>
+        </form>
+
+        {jsonData && (
+          <div className={styles.jsonDisplay}>
+            <h2>Submitted Data in (JSON):</h2>
+            <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default FormPage;
--- src/components/parents.jsx
+++ src/components/parents.jsx
@@ -2,27 +2,42 @@ import React, { useState } from "react";
 import Dialog from "@mui/material/Dialog"; // Importing Dialog from Material-UI
 import Stoppage from "./stoppage";
 import styles from "./parent.module.css";
+import FormPage from "./formPage";
 
 export function Parents() {
   const [openStoppageDialog, setOpenStoppageDialog] = useState(false);
+  const [openForm, setOpenForm] = useState(false);
 
   // Open dialog
   const openStoppage = () => {
     setOpenStoppageDialog(true);
   };
-
+  
   // Close dialog
   const closeStoppage = () => {
     setOpenStoppageDialog(false);
+  };
+
+  //Open form
+  const formPage = () => {
+    setOpenForm(true);
+  };
+
+  //Close form
+  const closeForm = () => {
+    setOpenForm(false);
   };
 
   return (
     <div>
       <div>
         <h2>I am the Parent component.</h2>
-        <button onClick={openStoppage}>Dialog-Popup</button>
+        <div className={styles.btnClick}>
+          <button onClick={openStoppage}>Dialog-Popup</button>
+          <button onClick={formPage}>Form Page</button>
+        </div>
       </div>
-      
+
       <Dialog
         open={openStoppageDialog}
         onClose={closeStoppage} // Handles dialog close
@@ -38,6 +53,23 @@ export function Parents() {
       >
         <div>
           <Stoppage closeDialog={closeStoppage} />
+        </div>
+      </Dialog>
+      <Dialog
+        open={openForm}
+        onClose={closeForm} // Handles dialog close
+        maxWidth="lg"
+        PaperProps={{
+          style: {
+            overflow: "visible",
+            minWidth: "800px", // Adjust width as needed
+            minHeight: "680px",
+            maxWidth: "800px",
+          },
+        }}
+      >
+        <div>
+          <FormPage closeDialog={closeForm} />
         </div>
       </Dialog>
     </div>
